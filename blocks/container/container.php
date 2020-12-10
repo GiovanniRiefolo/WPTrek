0357bedc9c2381b0cecf5c2352834cbdb18e51bc
Giovanni Riefolo <gioriefolo@gmail.com>
2020-12-10 10:08:59 +0100

    adding mobile box sizing feature for container block

--- blocks/container/container.php
+++ blocks/container/container.php
@@ -24,7 +24,11 @@ if( !empty($block['align']) ) {
 
 // Create class attribute values for Vulcan Blocks
 $scheme = '';
+$fullpage = '';
 $block_width = '';
+if(get_field('fullpagejs_enabled')){
+    $section .= sprintf('section');
+}
 if( get_field('color_scheme') == 'white' ) {
     $scheme .= sprintf('light-scheme');
 } elseif (get_field('color_scheme') == 'black' ){
@@ -37,101 +41,133 @@ if(get_field('content_width')){
  ?>
 
 
-<div id="<?php echo esc_attr($id); ?>" class="vc-container-block <?php echo esc_attr($classes); ?> <?php echo esc_attr($scheme); ?>"
+<div id="<?php echo esc_attr($id); ?>" class="vc-container-block <?php echo esc_attr($classes); ?> <?php echo esc_attr($scheme); ?> <?php echo esc_attr($fullpage); ?>"
      <?php if( get_field('bg_color') && get_field('global_bg')): ?>
      data-bg="<?php echo get_field('bg_color'); ?>"
      <?php endif; ?> >
 
-<style>
-    #<?php echo esc_attr($id); ?>{
-    <?php if( get_field('valign') == 'top' ) : ?>
-    align-items: flex-start;
-    <?php elseif( get_field('valign') == 'middle' ) : ?>
-    align-items: center;
-    <?php elseif( get_field('valign') == 'bottom' ) : ?>
-    align-items: flex-end;
-    <?php endif; ?>
-    <?php if( get_field('vheight') == 'fullheight' ) : ?>
-        height: 100vh;
-    <?php elseif( get_field('vheight') == 'halfheight' ) : ?>
-        height: 50vh;
-    <?php elseif( get_field('vheight') == 'thirdsheight' ) : ?>
-        height: 75vh;
-    <?php else : ?>
-        height: auto;
-    <?php endif; ?>
-    <?php if( get_field('bg_color') && get_field('global_bg')): ?>
-        background-color: transparent;
-        <?php else : ?>
-        background-color: <?php echo get_field('bg_color'); ?>;
-    <?php endif; ?>
-    <?php if( have_rows('margin') ): ?>
-    <?php while( have_rows('margin') ): the_row();
-        $mtop       = get_sub_field('margin_sup');
-        $mbottom    = get_sub_field('margin_inf');
-        $msx        = get_sub_field('margin_sx');
-        $mdx        = get_sub_field('margin_dx');
-        ?>
-        margin-top: <?php echo $mtop; ?>px;
-        margin-bottom: <?php echo $mbottom; ?>px;
-        margin-left: <?php echo $msx; ?>px;
-        margin-right: <?php echo $mdx; ?>px;
-    <?php endwhile; ?>
-    <?php else : ?>
-        margin-top: 0;
-        margin-bottom: 0;
-        margin-left: 0;
-        margin-right: 0;
-    <?php endif; ?>
-    <?php if( have_rows('padding') ): ?>
-    <?php while( have_rows('padding') ): the_row();
-        $ptop       = get_sub_field('padding_sup');
-        $pbottom    = get_sub_field('padding_inf');
-        $psx        = get_sub_field('padding_sx');
-        $pdx        = get_sub_field('padding_dx');
-        ?>
-        padding-top: <?php echo $ptop; ?>px;
-        padding-bottom: <?php echo $pbottom; ?>px;
-        padding-left: <?php echo $psx; ?>px;
-        padding-right: <?php echo $pdx; ?>px;
-    <?php endwhile; ?>
-    <?php else : ?>
-        padding-top: 64px;
-        padding-bottom: 64px;
-        padding-left: 0;
-        padding-right: 0;
-    <?php endif; ?>
-    }
-    #<?php echo esc_attr($id); ?> .vc-container-bg{
-    <?php if( get_field('bg_opacity') ): ?>
-        opacity: <?php echo get_field('bg_opacity'); ?>;
-    <?php endif; ?>
-    }
-
-   <?php if( get_field('bg_color') && get_field('global_bg')): ?>
+     <style>
         #<?php echo esc_attr($id); ?>{
-            background-color: <?php echo get_field('bg_color'); ?>
-            transition: background .3s ease-in-out;
+        <?php if( get_field('valign') == 'top' ) : ?>
+        align-items: flex-start;
+        <?php elseif( get_field('valign') == 'middle' ) : ?>
+        align-items: center;
+        <?php elseif( get_field('valign') == 'bottom' ) : ?>
+        align-items: flex-end;
+        <?php endif; ?>
+        <?php if( get_field('vheight') == 'fullheight' ) : ?>
+            height: 100vh;
+        <?php elseif( get_field('vheight') == 'halfheight' ) : ?>
+            height: 50vh;
+        <?php elseif( get_field('vheight') == 'thirdsheight' ) : ?>
+            height: 75vh;
+        <?php else : ?>
+            height: auto;
+        <?php endif; ?>
+        <?php if( get_field('bg_color') && get_field('global_bg')): ?>
+            background-color: transparent;
+            <?php else : ?>
+            background-color: <?php echo get_field('bg_color'); ?>;
+        <?php endif; ?>
+        <?php if( have_rows('margin') ): ?>
+        <?php while( have_rows('margin') ): the_row();
+            $mtop       = get_sub_field('margin_sup');
+            $mbottom    = get_sub_field('margin_inf');
+            $msx        = get_sub_field('margin_sx');
+            $mdx        = get_sub_field('margin_dx');
+            ?>
+            margin-top: <?php echo $mtop; ?>px;
+            margin-bottom: <?php echo $mbottom; ?>px;
+            margin-left: <?php echo $msx; ?>px;
+            margin-right: <?php echo $mdx; ?>px;
+        <?php endwhile; ?>
+        <?php else : ?>
+            margin-top: 0;
+            margin-bottom: 0;
+            margin-left: 0;
+            margin-right: 0;
+        <?php endif; ?>
+        <?php if( have_rows('padding') ): ?>
+        <?php while( have_rows('padding') ): the_row();
+            $ptop       = get_sub_field('padding_sup');
+            $pbottom    = get_sub_field('padding_inf');
+            $psx        = get_sub_field('padding_sx');
+            $pdx        = get_sub_field('padding_dx');
+            ?>
+            padding-top: <?php echo $ptop; ?>px;
+            padding-bottom: <?php echo $pbottom; ?>px;
+            padding-left: <?php echo $psx; ?>px;
+            padding-right: <?php echo $pdx; ?>px;
+        <?php endwhile; ?>
+        <?php else : ?>
+            padding-top: 64px;
+            padding-bottom: 64px;
+            padding-left: 0;
+            padding-right: 0;
+        <?php endif; ?>
         }
-     <?php if( get_field('color_scheme') == 'white' ) : ?>
-        #<?php echo esc_attr($id); ?>.inview *:not(.wp-block-button__link){
-         color: #fff !important;
-         border-color: #fff !important important;
+        #<?php echo esc_attr($id); ?> .vc-container-bg{
+        <?php if( get_field('bg_opacity') ): ?>
+            opacity: <?php echo get_field('bg_opacity'); ?>;
+        <?php endif; ?>
         }
-        #<?php echo esc_attr($id); ?>.inview svg path{
-         fill: #fff !important;
+
+        <?php if( get_field('bg_color') && get_field('global_bg')): ?>
+                #<?php echo esc_attr($id); ?>{
+                    background-color: <?php echo get_field('bg_color'); ?>
+                    transition: background .3s ease-in-out;
+                }
+                <?php if( get_field('color_scheme') == 'white' ) : ?>
+                #<?php echo esc_attr($id); ?>.inview *:not(.wp-block-button__link){
+                color: #fff;
+                border-color: #fff;
+                }
+                #<?php echo esc_attr($id); ?>.inview svg path{
+                fill: #fff;
+                }
+                <?php elseif( get_field('color_scheme') == 'black' ) : ?>
+                #<?php echo esc_attr($id); ?>.inview *:not(.wp-block-button__link{
+                color: #212121;
+                border-color: #212121;
+                }
+                #<?php echo esc_attr($id); ?>.inview svg path{
+                fill: #212121;
+                }
+            <?php endif; ?>
+        <?php endif; ?>
+
+        <?php if( have_rows('padding') ): ?>
+        <?php while( have_rows('padding') ): the_row();
+            $ptop_s      = get_sub_field('padding_sup_s');
+            $pbottom_s   = get_sub_field('padding_inf_s');
+            $psx_s       = get_sub_field('padding_sx_s');
+            $pdx_s       = get_sub_field('padding_dx_s');
+        ?>
+        @media screen and (max-width: 650px){
+        padding-top: <?php echo $ptop_s; ?>px;
+        padding-bottom: <?php echo $pbottom_s; ?>px;
+        padding-left: <?php echo $psx_s; ?>px;
+        padding-right: <?php echo $pdx_s; ?>px;
         }
-        <?php elseif( get_field('color_scheme') == 'black' ) : ?>
-        #<?php echo esc_attr($id); ?>.inview *:not(.wp-block-button__link{
-         color: #212121 !important;
-         border-color: #212121 !important;
+        <?php endwhile; ?>
+        <?php endif; ?>
+
+        <?php if( have_rows('margin') ): ?>
+        <?php while( have_rows('margin') ): the_row();
+        $mtop_s      = get_sub_field('margin_sup_s');
+        $mbottom_s   = get_sub_field('margin_inf_s');
+        $msx_s       = get_sub_field('margin_sx_s');
+        $mdx_s       = get_sub_field('margin_dx_s');
+        ?>
+        @media screen and (max-width: 650px){
+            margin-top: <?php echo $mtop_s; ?>px;
+            margin-bottom: <?php echo $mbottom_s; ?>px;
+            margin-left: <?php echo $msx_s; ?>px;
+            margin-right: <?php echo $mdx_s; ?>px;
         }
-        #<?php echo esc_attr($id); ?>.inview svg path{
-         fill: #212121;
-        }
-     <?php endif; ?>
-   <?php endif; ?>
-</style>
+        <?php endwhile; ?>
+        <?php endif; ?>
+    </style>
 
     <?php if( get_field('bg_image') ): ?>
     <div class="vc-container-bg">
@@ -140,7 +176,6 @@ if(get_field('content_width')){
         </figure>
     </div>
     <?php endif; ?>
-
      <div class="vc-content" style="<?php echo esc_attr($block_width); ?>">
         <InnerBlocks />
     </div>
